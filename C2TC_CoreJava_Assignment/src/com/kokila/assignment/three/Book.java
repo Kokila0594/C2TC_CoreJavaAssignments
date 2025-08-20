1edce52f1a99299a71b53f02293209db5cae7ce3
Kokila0594 <kokila090904@gmail.com>
2025-08-20 13:48:59 +0530

    Commit

---
+++ C2TC_CoreJava_Assignment/src/com/kokila/assignment/three/Book.java
@@ -0,0 +1,18 @@
+package com.kokila.assignment.three;
+
+public class Book extends Item {
+
+	private final String author;
+
+    public Book(int id, String title, String author) {
+        super(id, title);
+        this.author = author;
+    }
+
+    public String getAuthor() { return author; }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book: [ID: " + getId() + ", Title: " + getTitle() + ", Author: " + author + "]");
+    }
+}
