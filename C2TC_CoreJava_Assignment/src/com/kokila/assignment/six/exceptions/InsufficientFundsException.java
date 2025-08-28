1543cf38655758087f3b70f1e0a9afd0ebc3592b
Kokila0594 <kokila090904@gmail.com>
2025-08-28 20:58:34 +0530

    Commit

---
+++ C2TC_CoreJava_Assignment/src/com/kokila/assignment/six/exceptions/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package com.kokila.assignment.six.exceptions;
+
+public class InsufficientFundsException extends Exception {
+	public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
---
+++ C2TC_CoreJava_Assignment/src/com/kokila/assignment/six/exceptions/InvalidAmountException.java
@@ -0,0 +1,7 @@
+package com.kokila.assignment.six.exceptions;
+
+public class InvalidAmountException extends Exception {
+	public InvalidAmountException(String message) {
+        super(message);
+    }
+}
